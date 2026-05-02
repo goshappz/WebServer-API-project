b4cbfe8ac9a0661cb7378fc59ad79f1983f7fcef
goshappz <gepard249@yandex.ru>
2026-05-02 14:24:47 +0300

    Добавил регистрацию, создал базу данных для хранения логинов и паролей.

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from data import db_session
+from data.users import User
 import requests
 
 app = Flask(__name__)
+
+db_session.global_init("db/users.db")
+db_sess = db_session.create_session()
 
 
 @app.route('/')
@@ -9,6 +14,30 @@ def main():
     response = requests.get("https://dog.ceo/api/breeds/image/random").json()
     image_url = response['message']
     return render_template('index.html', image_url=image_url)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username'].strip()
+        email = request.form['email'].strip()
+        password = request.form['password'].strip()
+        terms = request.form['terms'].strip()
+        for user in db_sess.query(User).all():
+            if user.email == email:
+                return render_template('register.html', email_error="Этот адрес электронной почты уже используется")
+        user = User()
+        user.name = username
+        user.email = email
+        user.password = password
+        db_sess.add(user)
+        db_sess.commit()
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
